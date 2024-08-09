72c9b086ce79a8c97a5295f98e7529745c797ac3
vasan462001 <srini46200@gmail.com>
2024-08-09 10:17:00 +0530

    created

---
+++ password.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generating(length):
+    ch=string.ascii_letters+string.digits+string.punctuation
+
+    password=''.join(random.choice(ch) for _ in range(length))
+    return password
+def main():
+    length=int(input("enter the password length :"))
+    password=generating(length)
+    print("genaerting length",password)
+
+if __name__=="__main__":
+    main()
